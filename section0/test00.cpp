6cb42a31028d8c89afc35978d329f44e4ef684b2
chronolaw <chrono_cpp@me.com>
2020-01-16 17:15:25 +0800

    test00.cpp

--- section0/test00.cpp
+++ section0/test00.cpp
@@ -9,4 +9,10 @@ int main()
     using namespace std;
 
     cout << "c++ version = " << __cplusplus << endl;
+
+    cout << "gcc version = " << __VERSION__ << endl;
+
+    cout << "gcc major = " << __GNUC__ << endl;
+    cout << "gcc minor = " << __GNUC_MINOR__ << endl;
+    cout << "gcc patch = " << __GNUC_PATCHLEVEL__ << endl;
 }
