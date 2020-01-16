a659f8554a8222df371a8eff46510903e010d7d2
chronolaw <chrono_cpp@me.com>
2020-01-16 17:09:24 +0800

    test00.cpp

---
+++ section0/test00.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 by Chrono
+//
+// g++ test00.cpp -std=c++14 -o a.out;./a.out
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "c++ version = " << __cplusplus << endl;
+}
