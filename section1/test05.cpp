9aa4e5658f30e308317e51fad2676b745f607c0d
chronolaw <chrono_cpp@me.com>
2020-02-14 09:56:09 +0800

    test05.cpp

--- section1/test05.cpp
+++ section1/test05.cpp
@@ -6,22 +6,62 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include <set>
 
+#if 1
+
+class Interface {};
+
+class Implement final : public Interface
+{
+public:
+    using super_type = Interface;
+    using this_type  = Implement;
+private:
+};
+
+#endif
+
+#if 1
+
+struct KafkaConfig final
+{
+    int             id;
+    std::string     ip_addr;
+};
 
 class DemoClass final
 {
 public:
+    using this_type         = DemoClass;
+    using kafka_conf_type   = KafkaConfig;
+
+public:
     using string_type   = std::string;
     using uint32_type   = uint32_t;
+
     using set_type      = std::set<int>;
+    using vector_type   = std::vector<std::string>;
+
 public:
-  void set_name(const string_type& str);
+    DemoClass() = default;
+   ~DemoClass() = default;
+
+public:
+    void set_name(const string_type& str);
+
 private:
-  string_type   m_name;
-  uint32_type   m_age;
-  set_type      m_books;
+    string_type     m_name  = "tom";
+    uint32_type     m_age   = 23;
+    set_type        m_books;
+
+private:
+    kafka_conf_type m_conf;
 };
+
+#endif
+
 
 
 int main()
