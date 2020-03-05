20bbba73c364f9770556fcd06806b2f4013a8713
chronolaw <chrono_cpp@me.com>
2020-03-05 15:27:58 +0800

    test11.cpp

--- section3/test11.cpp
+++ section3/test11.cpp
@@ -100,12 +100,74 @@ public:
     }
 };
 
+static
+auto make_regex = [](const auto& txt)
+{
+    return std::regex(txt);
+};
+
+static
+auto make_match = []()
+{
+    return std::smatch();
+};
+
+void case5()
+{
+    using namespace std;
+
+    //using sregex = std::regex;
+    //using cregex = std::regex;
+
+    auto str = "neir:automata"s;
+
+    auto reg  = make_regex(R"(^(\w+)\:(\w+)$)");
+    auto what = make_match();
+
+    assert(regex_match(str, what, reg));
+
+    for(const auto& x : what) {
+        cout << x << ',';
+    }
+    cout << endl;
+}
+
+void case6()
+{
+    using namespace std;
+
+    auto str = "god of war"s;
+
+    auto reg  = make_regex(R"((\w+)\s(\w+))");
+    auto what = make_match();
+
+    auto found = regex_search(
+                    str, what, reg);
+
+    assert(found);
+    assert(!what.empty());
+    assert(what[1] == "god");
+    assert(what[2] == "of");
+
+    auto new_str = regex_replace(
+        str,
+        make_regex(R"(\w+$)"),
+        "peace"
+    );
+
+    cout << new_str << endl;
+}
+
+
+
 int main()
 {
     case1();
     case2();
     case3();
     case4();
+    case5();
+    case6();
 
     using namespace std;
 
