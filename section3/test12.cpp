541650c1a4dd132018be233d3d69f09527933792
chronolaw <chrono_cpp@me.com>
2020-03-06 13:52:58 +0800

    test12.cpp

---
+++ section3/test12.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 by Chrono
+//
+// g++ test12.cpp -std=c++11 -o a.out;./a.out
+// g++ test12.cpp -std=c++14 -o a.out;./a.out
+// g++ test12.cpp -std=c++14 -I../common -o a.out;./a.out
+
+#include <iostream>
+
+//#include "std.hpp"
+
+int main()
+{
+    using namespace std;
+
+    cout << "container demo" << endl;
+}
