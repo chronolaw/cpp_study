a506de935df5557e54ecd499be7b883e8db21dc6
chronolaw <chrono_cpp@me.com>
2020-03-10 14:37:02 +0800

    test14.cpp

--- section3/test14.cpp
+++ section3/test14.cpp
@@ -1,16 +1,42 @@
 // Copyright (c) 2020 by Chrono
 //
-// g++ test14.cpp -std=c++11 -o a.out;./a.out
-// g++ test14.cpp -std=c++14 -o a.out;./a.out
-// g++ test14.cpp -std=c++14 -I../common -o a.out;./a.out
+// g++ test14.cpp -lpthread -std=c++11 -o a.out;./a.out
+// g++ test14.cpp -lpthread -std=c++14 -o a.out;./a.out
+// g++ test14.cpp -lpthread -std=c++14 -I../common -o a.out;./a.out
 
 #include <iostream>
 
-//#include "std.hpp"
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+void case3()
+{
+    static once_flag flag;
+
+    auto f = []()
+    {
+        cout << "tid=" <<
+            this_thread::get_id() << endl;
+
+        std::call_once(flag,
+            [](){
+                cout << "only once" << endl;
+            }
+        );
+    };
+
+    thread t1(f);
+    thread t2(f);
+
+    t1.join();
+    t2.join();
+}
 
 int main()
 {
-    using namespace std;
+    case3();
 
     cout << "thread demo" << endl;
 }
