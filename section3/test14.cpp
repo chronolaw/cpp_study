c730b7ad8aa568f38a1e4bdee5f8b04bc071ece7
chronolaw <chrono_cpp@me.com>
2020-03-11 10:58:40 +0800

    test14.cpp

--- section3/test14.cpp
+++ section3/test14.cpp
@@ -15,27 +15,6 @@ using namespace std;
 
 void case1()
 {
-    //static int n = 0;
-    thread_local int n = 0;
-
-    auto f = [&](int x)
-    {
-        n += x;
-
-        cout << n;
-        cout << ", tid=" <<
-            this_thread::get_id() << endl;
-    };
-
-    thread t1(f, 10);
-    thread t2(f, 20);
-
-    t1.join();
-    t2.join();
-}
-
-void case2()
-{
     static once_flag flag;
 
     auto f = []()
@@ -56,6 +35,28 @@ void case2()
     t1.join();
     t2.join();
 }
+
+void case2()
+{
+    //static int n = 0;
+    thread_local int n = 0;
+
+    auto f = [&](int x)
+    {
+        n += x;
+
+        cout << n;
+        cout << ", tid=" <<
+            this_thread::get_id() << endl;
+    };
+
+    thread t1(f, 10);
+    thread t2(f, 20);
+
+    t1.join();
+    t2.join();
+}
+
 
 void case3()
 {
