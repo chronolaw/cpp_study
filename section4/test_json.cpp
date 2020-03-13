79f3d2d6b3e6be19050540901e1a82653153cb4b
chronolaw <chrono_cpp@me.com>
2020-03-13 16:31:22 +0800

    test_json.cpp

---
+++ section4/test_json.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 by Chrono
+//
+// g++ test_json.cpp -std=c++14 -I../common -o a.out;./a.out
+
+#include <iostream>
+
+#include "json.hpp"
+
+int main()
+{
+    using namespace std;
+
+    cout << "json demo" << endl;
+}
