2dd7ec42e4a5ff9790a23737502681a7636a2d71
chronolaw <chrono_cpp@me.com>
2020-03-13 16:43:39 +0800

    json

--- section4/test_json.cpp
+++ section4/test_json.cpp
@@ -6,9 +6,37 @@
 
 #include "json.hpp"
 
+using namespace std;
+
+using json_t = nlohmann::json;
+
+void case1()
+{
+    json_t j;
+    j["age"] = 23;
+    j["name"] = "spiderman";
+    j["jobs"] = {"superhero", "neighborhood"};
+    j["gear"]["suits"] = "2099";
+
+    cout << j.dump() << endl;
+}
+
+void case2()
+{
+    json_t j = {
+        {"age", 23},
+        {"name", "spiderman"},
+        {"jobs", {"superhero", "neighborhood"}},
+        {"gear", {"suits", "2099"}}
+    };
+
+    cout << j.dump(2) << endl;
+}
+
 int main()
 {
-    using namespace std;
+    case1();
+    case2();
 
     cout << "json demo" << endl;
 }
