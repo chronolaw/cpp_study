7f2e26084964b299faa685be9f9552dedcb731c1
chronolaw <chrono_cpp@me.com>
2020-03-13 22:08:43 +0800

    test_protobuf.cpp

---
+++ section4/test_protobuf.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2020 by Chrono
+//
+// sudo apt-get install protobuf-compiler
+// sudo apt-get install libprotobuf-dev
+// sudo apt-get install libprotoc-dev
+//
+// g++ test_protobuf.cpp -std=c++11 -lprotobuf -o a.out;./a.out
+// g++ test_protobuf.cpp -std=c++14 -lprotobuf -o a.out;./a.out
+// g++ test_protobuf.cpp -std=c++14 -lprotobuf -o a.out;./a.out
+
+#include <iostream>
+
+//#include "std.hpp"
+
+int main()
+{
+    using namespace std;
+
+    cout << "protobuf demo" << endl;
+}
