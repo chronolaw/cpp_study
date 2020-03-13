9aab0dd0a50b1725375243484c47ab58307a8b21
chronolaw <chrono_cpp@me.com>
2020-03-13 22:29:31 +0800

    test_zmq.cpp

---
+++ section4/test_zmq.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 by Chrono
+//
+// g++ test_zmq.cpp -std=c++11 -lzmq -o a.out;./a.out
+// g++ test_zmq.cpp -std=c++14 -lzmq -o a.out;./a.out
+
+#include <iostream>
+
+#include <zmq.hpp>
+
+int main()
+{
+    using namespace std;
+
+    cout << "zmq demo" << endl;
+}
