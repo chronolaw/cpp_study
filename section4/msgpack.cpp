46bb14dcc0b68c82d95d918342f0f548351d7bbb
chronolaw <chrono_cpp@me.com>
2020-03-14 14:53:38 +0800

    msgpack.cpp

--- section4/msgpack.cpp
+++ section4/msgpack.cpp
@@ -3,21 +3,68 @@
 // sudo apt-get install libmsgpack-dev
 //
 // /usr/include/msgpack
-// g++ msgpack.cpp -std=c++11 -lmsgpackc -o a.out;./a.out
-// g++ msgpack.cpp -std=c++14 -lmsgpackc -o a.out;./a.out
+// g++ msgpack.cpp -std=c++11 -o a.out;./a.out
+// g++ msgpack.cpp -std=c++14 -o a.out;./a.out
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 #include <msgpack.hpp>
 
+using namespace std;
+
 void case1()
 {
-    //msgpack::type::tuple<>
+    vector<int> v = {1,2,3,4,5};
+
+    msgpack::sbuffer sbuf;
+    msgpack::pack(sbuf, v);
+
+    cout << sbuf.size() << endl;
+
+    auto handle = msgpack::unpack(sbuf.data(), sbuf.size());
+    auto obj = handle.get();
+    cout << obj << endl;
+
+    decltype(v) v2;
+    obj.convert(v2);
+
+    assert(std::equal(begin(v), end(v), begin(v2)));
+}
+
+class Book final
+{
+public:
+    int         id;
+    string      title;
+    set<string> tags;
+public:
+    MSGPACK_DEFINE(id, title, tags);
+};
+
+void case2()
+{
+    Book book1 = {1, "1984", {"a","b"}};
+
+    msgpack::sbuffer sbuf;
+    msgpack::pack(sbuf, book1);
+
+    auto obj = msgpack::unpack(sbuf.data(), sbuf.size()).get();
+
+    Book book2;
+    obj.convert(book2);
+
+    assert(book2.id == book1.id);
+    assert(book2.tags.size() == 2);
+    cout << book2.title << endl;
 }
 
 int main()
 {
-    using namespace std;
+    case1();
+    case2();
 
     cout << "msgpack demo" << endl;
 }
