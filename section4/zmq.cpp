ee767ece3fc318916045b2b98c00b623aafc8929
chronolaw <chrono_cpp@me.com>
2020-03-15 11:45:38 +0800

    zmq.cpp

--- section4/zmq.cpp
+++ section4/zmq.cpp
@@ -1,30 +1,29 @@
 // Copyright (c) 2020 by Chrono
 //
-// g++ zmq.cpp -std=c++14 -lzmq -o a.out;./a.out
+// g++ zmq.cpp -std=c++14 -lzmq -lpthread -o a.out;./a.out
 
 #include <iostream>
 
 #include <string>
 #include <vector>
+#include <future>
 
 #include <zmq.hpp>
 
 using namespace std;
 
+const auto thread_num = 1;
+
+zmq::context_t context(thread_num);
+
+auto make_sock = [&](auto mode)
+{
+    return zmq::socket_t(context, mode);
+};
 
 void case1()
 {
-    //using namespace std::literals::string_literals;
-
-    const auto thread_num = 1;
     const auto addr = "ipc:///dev/shm/zmq.sock"s;
-
-    zmq::context_t context(thread_num);
-
-    auto make_sock = [&](auto mode)
-    {
-        return zmq::socket_t(context, mode);
-    };
 
     auto receiver = [=]()
     {
@@ -63,19 +62,52 @@ void case1()
 
 void case2()
 {
+    const auto addr = "tcp://127.0.0.1:5555"s;
+
     auto srv = [=]()
     {
+        auto sock = make_sock(ZMQ_PULL);
+
+        sock.bind(addr);
+        assert(sock.connected());
+
+        zmq::message_t msg;
+
+        for(int i = 0; i < 4; i++) {
+            sock.recv(&msg);
+
+            string s = {msg.data<char>(), msg.size()};
+            cout << s << endl;
+        }
     };
 
-    auto client = [=]()
+    auto client = [=](int i)
     {
+        auto sock = make_sock(ZMQ_PUSH);
+
+        sock.connect(addr);
+        assert(sock.connected());
+
+        string s = "zmq msg " + to_string(i);
+        //zmq::message_t msg(begin(s), end(s));
+        //sock.send(msg);
+
+        sock.send(begin(s), end(s));
     };
 
+    auto f = std::async(srv);
+
+    for(int i = 0; i < 4; i++) {
+        client(i);
+    }
+
+    f.wait();
 }
 
 int main()
 {
     case1();
+    case2();
 
     cout << "zmq demo" << endl;
 }
