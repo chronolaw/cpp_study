14206bec66f4ea40c82f481264fc2851088ca284
chronolaw <chrono_cpp@me.com>
2020-03-19 14:10:24 +0800

    luajit.cpp

--- section4/luajit.cpp
+++ section4/luajit.cpp
@@ -1,12 +1,29 @@
 // Copyright (c) 2020 by Chrono
 //
-// g++ luajit.cpp -std=c++11 -o a.out;./a.out
-// g++ luajit.cpp -std=c++14 -o a.out;./a.out
-// g++ luajit.cpp -std=c++14 -I../common -o a.out;./a.out
+// g++ luajit.cpp -std=c++11 -I/usr/local/include/luajit-2.1 -lluajit-5.1 -ldl -o a.out;./a.out
+//
+// g++ luajit.cpp -std=c++11 -I/usr/local/include/luajit-2.1 /usr/local/lib/libluajit-5.1.a -ldl -o a.out;./a.out
+// g++ luajit.cpp -std=c++11 -I/usr/local/include/luajit-2.1 -L/usr/local/lib -Wl,-rpath,/usr/local/lib -lluajit-5.1 -ldl -o a.out;./a.out
+//
 
+#include <cassert>
 #include <iostream>
 
-#include <luajit-2.1/luajit.h>
+extern "C" {
+#include <luajit.h>
+#include <lualib.h>
+#include <lauxlib.h>
+}
+
+void case1()
+{
+    auto L = luaL_newstate();
+    assert(L);
+
+    luaL_openlibs(L);
+
+    lua_close(L);
+}
 
 int main()
 {
