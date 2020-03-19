4b3fcbe0d2b1a98ea8de8728116318879707632b
chronolaw <chrono_cpp@me.com>
2020-03-19 14:40:53 +0800

    luajit.cpp

--- section4/luajit.cpp
+++ section4/luajit.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2020 by Chrono
 //
-// g++ luajit.cpp -std=c++11 -I/usr/local/include/luajit-2.1 -lluajit-5.1 -ldl -o a.out;./a.out
+// g++ luajit.cpp -std=c++11 -I../common -I/usr/local/include/luajit-2.1 -lluajit-5.1 -ldl -o a.out;./a.out
+// g++ luajit.cpp -std=c++11 -I../common -I/usr/local/include/luajit-2.1 -lluajit-5.1 -ldl -O0 -g -o a.out
 //
 // g++ luajit.cpp -std=c++11 -I/usr/local/include/luajit-2.1 /usr/local/lib/libluajit-5.1.a -ldl -o a.out;./a.out
 // g++ luajit.cpp -std=c++11 -I/usr/local/include/luajit-2.1 -L/usr/local/lib -Wl,-rpath,/usr/local/lib -lluajit-5.1 -ldl -o a.out;./a.out
@@ -15,6 +16,35 @@ extern "C" {
 #include <lauxlib.h>
 }
 
+#include <LuaBridge/LuaBridge.h>
+//namespace lua = luabridge;
+using namespace luabridge;
+
+using namespace std;
+
+class LuaState final
+{
+public:
+    using lua_state_t = decltype(luaL_newstate());
+private:
+    lua_state_t L = nullptr;
+public:
+    LuaState()
+    {
+        L = luaL_newstate();
+    }
+
+   ~LuaState()
+    {
+        lua_close(L);
+    }
+public:
+    operator lua_state_t() const
+    {
+        return L;
+    }
+};
+
 void case1()
 {
     auto L = luaL_newstate();
@@ -22,12 +52,18 @@ void case1()
 
     luaL_openlibs(L);
 
+    auto package = getGlobal(L, "package");
+
+    auto path = package["path"];
+
+    cout << path << endl;
+
     lua_close(L);
 }
 
 int main()
 {
-    using namespace std;
+    case1();
 
     cout << "luajit demo" << endl;
 }
