0537ff983b091a10882cfa82301f5e4544807cda
chronolaw <chrono_cpp@me.com>
2020-04-09 22:09:47 +0800

    config.lua

---
+++ section5/config.lua
@@ -0,0 +1,18 @@
+-- copyright (c) 2020 chrono
+
+-- it is more flexible than json
+config = {
+
+    max_buf_size = 4 * 1024,
+
+    keyword = "super",
+
+    sold_criteria = 100,
+
+    revenue_criteria = 1000,
+
+    best_n  = 3,
+
+    time_interval = 1,
+
+}
