934fdd99b19fc866ee8ad9b4cf44f20ec66de598
chronolaw <chrono_cpp@me.com>
2020-04-10 11:03:17 +0800

    hello.hpp

---
+++ section5/hello.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 by Chrono
+
+#ifndef _HELLO_HPP
+#define _HELLO_HPP
+
+#include "cpplang.hpp"
+
+BEGIN_NAMESPACE(cpp_study)
+
+
+END_NAMESPACE(cpp_study)
+
+#endif  //_HELLO_HPP
+
