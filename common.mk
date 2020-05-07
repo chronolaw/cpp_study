eceb60d1556976d082e73d553cd0b33dca88723a
Ji Bin <matrixji@live.com>
2020-05-07 00:56:47 +0000

    Feature: add Makefile for section0-3
    - a common.mk rules makefile for section0-3
    
    Signed-off-by: Ji Bin <matrixji@live.com>

---
+++ common.mk
@@ -0,0 +1,15 @@
+CXX_FLAGS=-Wall
+
+all: examples
+
+example_%_cpp14: %.cpp
+	g++ -std=c++14 -o $@ $^ $(CXX_FLAGS) $($@_cxx_flags) $($@_ld_flags) 
+
+example_%_cpp11: %.cpp
+	g++ -std=c++14 -o $@ $^ $(CXX_FLAGS) $($@_cxx_flags) $($@_ld_flags)
+
+example_%_cpp98: %.cpp
+	g++ -std=c++98 -o $@ $^ $(CXX_FLAGS) $($@_cxx_flags) $($@_ld_flags)
+
+clean:
+	rm -fr example_*
