85891d97fd595b358dee000dc44c58a9d92b2d19
chronolaw <chrono_cpp@me.com>
2020-05-07 13:52:29 +0800

    fix c++11 in makefile

--- common.mk
+++ common.mk
@@ -6,7 +6,7 @@ example_%_cpp14: %.cpp
 	g++ -std=c++14 -o $@ $^ $(CXX_FLAGS) $($@_cxx_flags) $($@_ld_flags) 
 
 example_%_cpp11: %.cpp
-	g++ -std=c++14 -o $@ $^ $(CXX_FLAGS) $($@_cxx_flags) $($@_ld_flags)
+	g++ -std=c++11 -o $@ $^ $(CXX_FLAGS) $($@_cxx_flags) $($@_ld_flags)
 
 example_%_cpp98: %.cpp
 	g++ -std=c++98 -o $@ $^ $(CXX_FLAGS) $($@_cxx_flags) $($@_ld_flags)
